1e9a9f6ffb3f9239c59ac1c97d8d1c4fd9322799
hiennguyen-corize <115438418+hiennguyen-corize@users.noreply.github.com>
2025-02-24 01:55:44 +0700

    Change animation

--- src/components/ui/BottomSheet/BottomSheet.tsx
+++ src/components/ui/BottomSheet/BottomSheet.tsx
@@ -36,7 +36,8 @@ export const BottomSheet: FC<BottomSheetProps> = ({
           {/* Backdrop */}
           <motion.div
             initial={{ opacity: 0 }}
-            animate={{ opacity: 0.5 }}
+            animate={{ opacity: 0.8 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
             exit={{ opacity: 0 }}
             className="fixed inset-0 bg-black z-50 md:hidden"
             onClick={onClose}
@@ -46,7 +47,13 @@ export const BottomSheet: FC<BottomSheetProps> = ({
             initial={{ y: "100%" }}
             animate={{ y: 0 }}
             exit={{ y: "100%" }}
-            transition={{ type: "spring", damping: 25, stiffness: 300 }}
+            transition={{
+              type: "spring",
+              damping: 25,
+              ease: "easeOut",
+              stiffness: 300,
+              duration: 0.25,
+            }}
             className="fixed bottom-0 left-0 right-0 bg-white rounded-t-lg p-4 max-h-[80vh] overflow-y-auto z-50 md:hidden"
             onClick={(e) => e.stopPropagation()}
           >
