b44e9af3bf0683aff566bda175a47d2041aa1b4a
hiennguyen-corize <115438418+hiennguyen-corize@users.noreply.github.com>
2025-02-25 18:03:53 +0700

    Delete link

--- src/components/ui/HelmetCustom/HelmetCustom.tsx
+++ src/components/ui/HelmetCustom/HelmetCustom.tsx
@@ -51,7 +51,6 @@ export const HelmetCustom: FC<Props> = ({
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
-      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
 
